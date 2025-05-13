44a4b082e79b6b994e559171f4bb6cf32b04122d
strl3n <fredley.infor@gmail.com>
2025-05-13 12:28:09 +0700

    Tampilan Login dan pengecekan sederhana (Field email dan password tidak boleh kosong)

--- app/src/main/java/com/istts/healthforyou/LoginFragment.kt
+++ app/src/main/java/com/istts/healthforyou/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.istts.healthforyou.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -14,6 +15,14 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val email = binding.loginEmail.text.toString()
+        val password = binding.loginPassword.text.toString()
+
+        binding.btnLogin.setOnClickListener(){
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(requireContext(), "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return binding.root
     }
