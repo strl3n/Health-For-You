5e202c3419d7fac1776923a75b469e0e3822d831
strl3n <fredley.infor@gmail.com>
2025-05-14 15:40:11 +0700

    Login done, next register

--- app/src/main/java/com/istts/healthforyou/LoginFragment.kt
+++ app/src/main/java/com/istts/healthforyou/LoginFragment.kt
@@ -5,22 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.istts.healthforyou.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    private lateinit var binding: FragmentLoginBinding // Inisialisasi untuk binding LoginFragment
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val email = binding.loginEmail.text.toString()
-        val password = binding.loginPassword.text.toString()
 
-        binding.btnLogin.setOnClickListener(){
-            if(email.isEmpty() || password.isEmpty()){
-                Toast.makeText(requireContext(), "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+        binding.btnLogin.setOnClickListener {
+            // Dapatkan nilai email dan password saat tombol diklik
+            val email = binding.loginEmail.text.toString().trim()
+            val password = binding.loginPassword.text.toString().trim()
+
+            // Reset semua error
+            binding.notificationErrorEmail.visibility = View.GONE
+            binding.notificationErrorPassword.visibility = View.GONE
+
+            // Validasi input
+            when {
+                email.isEmpty() && password.isEmpty() -> {
+                    binding.notificationErrorEmail.text = "Email harus diisi"
+                    binding.notificationErrorEmail.visibility = View.VISIBLE
+                    binding.notificationErrorPassword.text = "Password harus diisi"
+                    binding.notificationErrorPassword.visibility = View.VISIBLE
+                }
+                email.isEmpty() -> {
+                    binding.notificationErrorEmail.text = "Email harus diisi"
+                    binding.notificationErrorEmail.visibility = View.VISIBLE
+                }
+                password.isEmpty() -> {
+                    binding.notificationErrorPassword.text = "Password harus diisi"
+                    binding.notificationErrorPassword.visibility = View.VISIBLE
+                }
+                else -> {
+
+                }
             }
         }
 
