3de8aa608a99c5060191f4b896955b7d2bf93bbb
strl3n <fredley.infor@gmail.com>
2025-05-16 15:41:55 +0700

    LOGIN, REGISTER, MENU DONE, NEXT PRINTILANNYA

--- app/src/main/java/com/istts/healthforyou/MainFragment.kt
+++ app/src/main/java/com/istts/healthforyou/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.istts.healthforyou.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -14,6 +15,22 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        binding.btnKontrolKesehatan.setOnClickListener(){
+            Toast.makeText(requireContext(), "Kontrol Kesehatan", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLayananRehabilitasi.setOnClickListener(){
+            Toast.makeText(requireContext(), "Layanan Rehabilitasi", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnSeminarKesehatan.setOnClickListener(){
+            Toast.makeText(requireContext(), "Seminar Kesehatan", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnKonsultasi.setOnClickListener(){
+            Toast.makeText(requireContext(), "Layanan Rehabilitasi", Toast.LENGTH_SHORT).show()
+        }
 
         return binding.root
     }
